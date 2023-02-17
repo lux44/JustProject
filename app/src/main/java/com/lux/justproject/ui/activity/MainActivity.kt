142dcaa639ab8fc4ad049bc4f8853f02e958ec18
lux44 <couquedas14@gmail.com>
2023-02-17 16:23:24 +0900

    Intro Animation 시도중 자바 작성해야함/테스트도

--- app/src/main/java/com/lux/justproject/ui/activity/MainActivity.kt
+++ app/src/main/java/com/lux/justproject/ui/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.lux.justproject.ui.activity
 
+import android.view.animation.AnimationUtils
 import com.lux.justproject.R
 import com.lux.justproject.databinding.ActivityMainBinding
 
@@ -8,8 +9,13 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     override val layoutResID: Int
         get() = R.layout.activity_main
 
-    override fun afterOnCreate() {
+    private val heartAni by lazy { AnimationUtils.loadAnimation(this@MainActivity,R.anim.logo) }
 
+
+    override fun afterOnCreate() {
+        bind{
+
+        }
     }
 
 }
